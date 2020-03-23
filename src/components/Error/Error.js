77b8eef99add2daed5ff669cbc3bcdd37ddfb711
SolodkayaT <tatasladkaya@gmail.com>
2020-03-23 20:40:42 +0200

    add error component and styles

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,5 +1,6 @@
 import React from "react";
+import styles from "./Error.module.css";
 
 export default function Error({ message }) {
-  return <p>{message}</p>;
+  return <p className={styles.errorMessage}>{message}</p>;
 }
