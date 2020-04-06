13b06e678371a15e05430f90ce52a3c493648c32
SolodkayaT <tatasladkaya@gmail.com>
2020-04-06 18:16:23 +0300

    add new api

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,35 +1,35 @@
 import contactsActions from "./contactsActions";
 import axios from "axios";
-axios.defaults.baseURL = "https://goit-phonebook-api.herokuapp.com/v1";
+axios.defaults.baseURL = "https://goit-phonebook-api.herokuapp.com";
 axios.defaults.headers.common["Authorization"] =
-  "1474f3f5-f023-4759-aec0-68582b03d82b";
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZThiNDZlNmMzNWY3ODAwMTc4ZDI0MDYiLCJpYXQiOjE1ODYxODU5NTh9.bOcrq6tidpidkBygmcJo9ENxVQzWGJY7WI18mvuW8Pc";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-const addContact = (name, number) => dispatch => {
+const addContact = (name, number) => (dispatch) => {
   dispatch(contactsActions.addContactRequest());
   axios
     .post("/contacts", { name, number })
     .then(({ data }) => {
       dispatch(contactsActions.addContactSuccess(data));
     })
-    .catch(error => dispatch(contactsActions.addContactError(error)));
+    .catch((error) => dispatch(contactsActions.addContactError(error)));
 };
 
-const fetchContacts = () => dispatch => {
+const fetchContacts = () => (dispatch) => {
   dispatch(contactsActions.fetchContactRequest());
   axios
     .get("/contacts")
     .then(({ data }) => dispatch(contactsActions.fetchContactSuccess(data)))
-    .catch(error => contactsActions.fetchContactError(error));
+    .catch((error) => contactsActions.fetchContactError(error));
 };
 
-const removeContact = id => dispatch => {
+const removeContact = (id) => (dispatch) => {
   dispatch(contactsActions.removeContactRequest());
 
   axios
     .delete(`/contacts/${id}`)
     .then(() => dispatch(contactsActions.removeContactSuccess(id)))
-    .catch(error => dispatch(contactsActions.fetchContactError(error)));
+    .catch((error) => dispatch(contactsActions.fetchContactError(error)));
 };
 
 export default { addContact, fetchContacts, removeContact };
